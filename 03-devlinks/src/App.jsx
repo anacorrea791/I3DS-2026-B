37759a65d28b442df2e67689d57081f5e9c12d4d
anacorrea791 <correaanaclara898@gmail.com>
2026-02-12 17:00:00 -0300

    feat: adicionar componentes Link, Rodape e SocialLink; atualizar estrutura do componente Perfil

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,21 +1,32 @@
 import './App.css'
+import Link from './components/Link/Link'
 import Perfil from './components/Perfil/Perfil'
+import Rodape from './components/Rodape/Rodape'
+import SocialLink from './components/SocialLink/SocialLink'
 
 function App() {
   return (
+
     <div id="App">
 
-      <Perfil />
-      <div className='perfil'>
-        <img src="https://placehold.co/200x200"  alt="" />
-        <p>@Ana Clara</p>
+      <Perfil fotoPerfil={"https://placehold.co/100"}> Ana Clara</Perfil>
+
+      <div className="switch"> botão switch</div>
+
+      <ul>
+      <Link url={""}>Inscreva-se</Link>
+      <Link url={""}>Minha playlist</Link>
+      <Link url={""}>Me pegue um café!</Link>
+      <Link url={""}>Conheça o curso DEV</Link>
+      </ul>
+
+      <div className="socialLinks">
+        <SocialLink url={"https://github.com"} icon={"logo-github"} />
+        <SocialLink url={"https://instagram.com"} icon={"logo-instagram"} />
+        <SocialLink url={"https://youtube.com"} icon={"logo-youtube"} />
+        <SocialLink url={"https://br.linkedin.com/"} icon={"logo-linkedin"} />
       </div>
-      <div className="switch">
-        botão switch
-      </div>
-      <div className="links"></div>
-      <div className="socialLinks"></div>
-      <div className="rodape"></div>
+      <Rodape>Ana Clara</Rodape>
     </div>
   )
 }
