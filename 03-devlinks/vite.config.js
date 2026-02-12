6e8b12a6ea35bfc267fbd673ecf884d6acade8f9
anacorrea791 <correaanaclara898@gmail.com>
2026-02-12 11:30:39 -0300

    feat: refatorar estrutura do App e remover estilos não utilizados

--- 03-devlinks/vite.config.js
+++ 03-devlinks/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server : {
+    port : 3000, //muda a porta padrão vite(5173) para a padrão react (3000)
+    open : true, //abre o navegador automaticamente
+    host : true, //permite que tenha acesso via IP na rede local
+  },
 })
