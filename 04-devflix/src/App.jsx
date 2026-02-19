b24e1709c1aad2d8224e416c5fdf4d842cfa22a0
anacorrea791 <correaanaclara898@gmail.com>
2026-02-19 17:01:05 -0300

    feat: adicionar lógica de busca de filmes e atualizar componentes MovieCard e App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
 
 import logo from "./assets/devflix.png";
 import lupa from "./assets/search.svg";
 import Rodape from "./components/Rodape/Rodape";
 
+
 const App = () => {
+  const [movies, setMovies] = useState ([]);
+
+  //Utilizando uma CHAVE de API do arquivo .env
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
+  //Criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}`);
+    const data = await response.json;
+
+    //Alimentando a variavel movies
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
 
   return (
     <div id="App">
@@ -17,6 +37,12 @@ const App = () => {
       <div className="search">
         <input type="text" placeholder="Pesquise por filmes e séries..." />
         <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+      <div className="container">
+        {movies.map((movie, index) => (
+          <movie
+        ))}
       </div>
 
       <Rodape link={"https://github.com/anacorrea791"}>Ana clara</Rodape>
