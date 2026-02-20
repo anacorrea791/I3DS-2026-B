fc71a84639d8e9b2ba668e9e3921387f9c4d37e4
anacorrea791 <correaanaclara898@gmail.com>
2026-02-20 16:36:35 -0300

    feat: adicionar lógica de modal e componente MovieDescription ao MovieCard

--- 04-devflix/src/components/MovieCard/MovieCard.jsx
+++ 04-devflix/src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import styles from "./MovieCard.module.css";
+import MovieDescription from "../MovieDescription/MovieDescription";
 
 const MovieCard = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  //console.log(isModalOpen)
+  const toggleModal = () => { setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <>
-      <div className={styles.movie}>
+      <div className={styles.movie} onClick={toggleModal}>
         <div>
           <p>{props.Year}</p>
         </div>
@@ -17,6 +24,10 @@ const MovieCard = (props) => {
           <h3>{props.Title}</h3>
         </div>
       </div>
+
+       {isModalOpen && (
+        <MovieDescription apiUrl={props.apiUrl} movieID={props.imdbID} click={toggleModal} />
+      )}
     </>
   );
 };
