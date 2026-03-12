405af8dfc7a27c2e0cc7d4c847806ef55c9f1e4e
anacorrea791 <correaanaclara898@gmail.com>
2026-03-12 16:53:43 -0300

    criando outro projeto em bootstrap

---
+++ 05-react-router-pro-max/src/components/rodape/Rodape.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rodape = () => {
+  return (
+    <div>
+       Feito com 🤍 por Ana Clara
+    </div>
+  )
+}
+
+export default Rodape
---
+++ 05-react-router-pro-max/src/pages/NãoEncontrado.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NãoEncontrado = () => {
+  return (
+    <h1> Não Encontrado</h1>
+  )
+}
+
+export default NãoEncontrado
