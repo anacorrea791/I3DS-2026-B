84be7299e2c49d9c4dca4b99e222803763ca25f0
anacorrea791 <correaanaclara898@gmail.com>
2026-03-12 16:26:39 -0300

    novas páginas  ao projeto,

--- 05-react-router/src/App.jsx
+++ 05-react-router/src/App.jsx
@@ -4,6 +4,9 @@ import './App.css';
 import Sobre from './pages/Sobre';
 import Home from './pages/Home';
 import Header from './components/header/Header'
+import Contato from './pages/Contato'
+import NãoEncontrado from './pages/NãoEncontrado';
+import Rodape from './components/rodape/Rodape'
 
 function App() {
   
@@ -11,10 +14,17 @@ function App() {
   return (
     <>
     <Header />
+    
+    <div className='containerApp'>
      <Routes> {/* Identifica todas as rotas do sistema  */}
       <Route path = "/" element={<Home />} />
       <Route path = "/sobre" element={<Sobre />} /> {/* uma rota do sistema */}
+      <Route path = "/contato" element={<Contato />} />
+      <Route path = "/nãoencontrado" element={<NãoEncontrado />} />
      </Routes>
+     </div>
+
+      <Rodape >Ana clara</Rodape>
     </>
   );
 }
--- 05-react-router/src/components/header/Header.jsx
+++ 05-react-router/src/components/header/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from "react";
+import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <nav className='navbar'>
-        <div className='container'>
-            <Link to = "/">Home</Link>
-            <Link to = "/sobre">Sobre</Link> {/*uma rota de sistema*/}
-        </div>
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/">Home</Link>
+        <Link to="/sobre">Sobre</Link> {/*uma rota de sistema*/}
+        <Link to="/contato">Contato</Link>
+        <Link to="/nãoencontrado">NãoEncontrado</Link>
+      </div>
     </nav>
   );
 };
 
-export default Header
+export default Header;
