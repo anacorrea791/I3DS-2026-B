57126a254acd0a0d4ecfaa1bf82cd4fb9940945a
anacorrea791 <correaanaclara898@gmail.com>
2026-04-24 08:07:22 -0300

    fix: corrige a conexão do socket e melhora o foco no campo de mensagem

--- 06-devChat/client/src/components/Chat/Chat.jsx
+++ 06-devChat/client/src/components/Chat/Chat.jsx
@@ -7,6 +7,8 @@ import { useEffect, useRef, useState } from "react";
 const Chat = (props) => {
   const [messageList, setMessageList] = useState([]);
 
+  const bottomRef = useRef();
+
   const messageRef = useRef();
 
   useEffect(() => {
@@ -16,6 +18,8 @@ const Chat = (props) => {
       // usa função callback para garantir que pega o estado mais recente
       setMessageList((current) => [...current, data]);
     });
+
+    messageRef.current.focus();
 
     // Cleanup: remover o listener quando o componente desmonta
     // Evita vazamento de memória e listeners duplicados
@@ -30,11 +34,19 @@ const Chat = (props) => {
     props.socket.emit("message", message);
 
     messageRef.current.value = "";
-    message.Ref.current.focus();
+    messageRef.current.focus();
   };
 
   const getEnterKey = (e) => {
     if (e.key === "Enter") handleSubmit();
+  };
+
+  useEffect(() => {
+    scrollDown();
+  }, [messageList]);
+
+  const scrollDown = () => {
+    bottomRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
@@ -52,6 +64,8 @@ const Chat = (props) => {
               <div className={style.message_text}>{message.text}</div>
             </div>
           ))}
+
+          <div ref={bottomRef} />
         </div>
 
         <div className={style.chat_footer}>
